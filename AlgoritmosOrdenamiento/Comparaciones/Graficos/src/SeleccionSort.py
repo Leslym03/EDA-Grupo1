70902853ccc269e8b8fc6c1c3a10cbc1f2ba6047
Leslym03 <leslymita03@gmail.com>
2020-09-26 17:15:10 -0500

    img

--- AlgoritmosOrdenamiento/Comparaciones/Graficos/src/SeleccionSort.py
+++ AlgoritmosOrdenamiento/Comparaciones/Graficos/src/SeleccionSort.py
@@ -3,17 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 x,y = np.loadtxt('Medidas/SeleccionSortcc.txt' , delimiter=',' , unpack=True)
-x1,y1 = np.loadtxt('Medidas/SeleccionSortpy.txt' , delimiter=',' , unpack=True)
-x2,y2 = np.loadtxt('Medidas/SeleccionSortjav.txt' , delimiter=',' , unpack=True)
+x1,y1 = np.loadtxt('Medidas/SeleccionSortjav.txt' , delimiter=',' , unpack=True)
+x2,y2 = np.loadtxt('Medidas/SeleccionSortpy.txt' , delimiter=',' , unpack=True)
 
-
-plt.plot(x,y*1000,color="blue",label="Seleccion Sort en C++")
-plt.plot(x1,y1*1000,color="yellow",label="Seleccion Sort en Python")
-plt.plot(x2,y2*1000,color="red",label="Seleccion Sort en Java")
+plt.plot(x,y,color="blue",label="Seleccion Sort en C++")
+plt.plot(x1,y1,color="yellow",label="Seleccion Sort en Java")
+plt.plot(x2,y2,color="red",label="Seleccion Sort en Python")
 
 plt.legend()
-plt.title('Algoritmo de  Selection')
+plt.title('Algoritmo SelectionSort')
 plt.xlabel('Cantidad de datos')
-plt.ylabel('Tiempo de ejecucion (en milisegundos)')
-plt.text(300000,50000000,r'$O(n^2)$', fontsize=12)
+plt.ylabel('Tiempo de ejecucion (segundos)')
+plt.text(220000,55000,r'$O(n^2)$', fontsize=12)
 plt.show()
