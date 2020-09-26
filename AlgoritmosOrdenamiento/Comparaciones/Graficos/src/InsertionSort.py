295f3d40cb19ecfacdc86859e21287dce230fcfd
Leslym03 <leslymita03@gmail.com>
2020-09-26 17:24:50 -0500

    img

---
+++ AlgoritmosOrdenamiento/Comparaciones/Graficos/src/InsertionSort.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+x1,y1 = np.loadtxt('Medidas/InsertionSortcc.txt' , delimiter=' ' , unpack=True)
+x2,y2 = np.loadtxt('Medidas/InsertionSortjav.txt' , delimiter=' ' , unpack=True)
+x3,y3 = np.loadtxt('Medidas/InsertionSortpy.txt' , delimiter=' ' , unpack=True)
+
+plt.plot(x1,y1*6,color="blue",label="InsertionSort en C++")
+plt.plot(x2,y2*6,color="yellow",label="InsertionSort en  Java")
+plt.plot(x3,y3*6,color="red",label="InsertionSort en  Python")
+
+plt.legend()
+plt.title('Algoritmo InsertionSort')
+plt.xlabel('Cantidad de datos')
+plt.ylabel('Tiempo de ejecucion (segundos)')
+plt.text(240000,11000,r'$O(n^2)$', fontsize=12)
+plt.show()
